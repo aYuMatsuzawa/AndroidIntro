5071e5453fc31c6890cf040d68b1ca5565429aac
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2015-04-24 16:09:44 +0900

    Showing ListView. Button/Canvas UI sorted out.

--- src/main/java/com/example/yumatsuzawa/myapplication/MainActivity.java
+++ src/main/java/com/example/yumatsuzawa/myapplication/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -15,29 +18,51 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+//        setContentView(R.layout.activity_main);
+//
+//        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Yo Men!", Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        Button button2 = (Button)findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+//                        .setMessage("Hey Jude")
+//                        .setPositiveButton("Don't make it bad", null)
+//                        .create();
+//                alertDialog.show();
+//            }
+//        });
+//
+//        HelloWorldView hwView = new HelloWorldView(this);
+//        setContentView(hwView); // setContentView replaces RootView of this app.
+
+        ListView lsView = new ListView(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        lsView.setAdapter(adapter);
+
+        adapter.add("Alpha");
+        adapter.add("Beta");
+        adapter.add("Charlie");
+
+        lsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Yo Men!", Toast.LENGTH_LONG).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_LONG).show();
             }
         });
 
-        Button button2 = (Button)findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
-                        .setMessage("Hey Jude")
-                        .setPositiveButton("Don't make it bad", null)
-                        .create();
-                alertDialog.show();
-            }
-        });
-
-        HelloWorldView hwView = new HelloWorldView(this);
-        setContentView(hwView);
+        setContentView(lsView);
     }
 
 
